9bd29ebd887b229961daa7c0b7353ad23b9b3561
itikatyagi <itikatyagi60@gmail.com>
2025-10-12 00:11:55 +0100

    test: add failing tests for Grid boundaries + input parser interface

---
+++ MartianRobots.Tests/GridTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using MartianRobots.Models;
+
+namespace MartianRobots.Tests
+{
+    public class GridTests
+    {
+        [Fact]
+        public void IsOutOfBounds_ReturnsTrue_ForCoordinatesOutside()
+        {
+            var grid = new Grid(5, 3);
+            Assert.True(grid.IsOutOfBounds(6, 2));
+            Assert.True(grid.IsOutOfBounds(0, 4));
+            Assert.True(grid.IsOutOfBounds(-1, 0));
+        }
+
+        [Fact]
+        public void IsOutOfBounds_ReturnsFalse_ForBoundaryAndInside()
+        {
+            var grid = new Grid(5, 3);
+            Assert.False(grid.IsOutOfBounds(0, 0));
+            Assert.False(grid.IsOutOfBounds(5, 3));
+        }
+    }
+}
---
+++ MartianRobots.Tests/ParserTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using MartianRobots.Parsing;
+using System.IO;
+using System.Text;
+
+namespace MartianRobots.Tests
+{
+    public class ParserTests
+    {
+        [Fact]
+        public void Parser_Interface_Should_ParseGridAndRobotBlocks_FromText()
+        {
+            var input = new StringBuilder();
+            input.AppendLine("5 3");
+            input.AppendLine("1 1 E");
+            input.AppendLine("RFRFRFRF");
+            using var sr = new StringReader(input.ToString());
+
+            IInputParser parser = new SimpleTextParser();
+            var result = parser.Parse(sr);
+
+            Assert.Equal((5, 3), result.GridSize);
+            Assert.Single(result.Robots);
+        }
+    }
+}
