706aa14d1841c0b9d8df50026302362f56444e9d
itikatyagi <itikatyagi60@gmail.com>
2025-10-12 00:57:35 +0100

    feat: implement Direction strategy

---
+++ MartianRobots/Models/Directions/IDirection.cs
@@ -0,0 +1,11 @@
+﻿namespace MartianRobots.Models.Directions
+{
+    /// <summary>Directional behavior for robots.</summary>
+    public interface IDirection
+    {
+        char Name { get; }
+        IDirection TurnLeft();
+        IDirection TurnRight();
+        (int dx, int dy) MoveVector();
+    }
+}
---
+++ MartianRobots/Models/Directions/NorthDirection.cs
@@ -0,0 +1,10 @@
+﻿namespace MartianRobots.Models.Directions
+{
+    public class NorthDirection : IDirection
+    {
+        public char Name => 'N';
+        public IDirection TurnLeft() => new WestDirection();
+        public IDirection TurnRight() => new EastDirection();
+        public (int dx, int dy) MoveVector() => (0, 1);
+    }
+}
