5bf0993f70b82106fd506f21858258ededf094ad
itikatyagi <itikatyagi60@gmail.com>
2025-10-12 00:37:01 +0100

    test: add failing tests for Direction strategies and robot basics

---
+++ MartianRobots.Tests/DirectionTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using MartianRobots.Models.Directions;
+
+namespace MartianRobots.Tests
+{
+    public class DirectionTests
+    {
+        [Fact]
+        public void North_Turns_Left_To_West_And_Right_To_East()
+        {
+            var n = new NorthDirection();
+            Assert.IsType<WestDirection>(n.TurnLeft());
+            Assert.IsType<EastDirection>(n.TurnRight());
+        }
+
+        // other directional tests...
+    }
+}
---
+++ MartianRobots.Tests/RobotBasicTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using MartianRobots.Models;
+using MartianRobots.Models.Directions;
+
+namespace MartianRobots.Tests
+{
+    public class RobotBasicTests
+    {
+        [Fact]
+        public void Robot_Turns_Left_Right()
+        {
+            var robot = new Robot(0, 0, new NorthDirection());
+            robot.TurnRight();
+            Assert.Equal('E', robot.Direction.Name);
+            robot.TurnLeft();
+            Assert.Equal('N', robot.Direction.Name);
+        }
+    }
+}
