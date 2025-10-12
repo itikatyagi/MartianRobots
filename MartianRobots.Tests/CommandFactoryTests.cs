cd758eb8265d9e08c0df360cc4dd31cf94c69e7c
itikatyagi <itikatyagi60@gmail.com>
2025-10-12 02:01:33 +0100

    test: add failing tests for Command pattern and CommandFactory

---
+++ MartianRobots.Tests/CommandFactoryTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+using MartianRobots;
+
+namespace MartianRobots.Tests
+{
+    public class CommandFactoryTests
+    {
+        [Fact]
+        public void Factory_Creates_CorrectCommandInstances()
+        {
+            Assert.IsType<Commands.TurnLeftCommand>(CommandFactory.Create('L'));
+            Assert.IsType<Commands.TurnRightCommand>(CommandFactory.Create('R'));
+            Assert.IsType<Commands.MoveForwardCommand>(CommandFactory.Create('F'));
+        }
+    }
+}
---
+++ MartianRobots.Tests/CommandTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+using MartianRobots.Models;
+using MartianRobots.Models.Directions;
+using MartianRobots.Commands;
+
+namespace MartianRobots.Tests
+{
+    public class CommandTests
+    {
+        [Fact]
+        public void TurnCommands_ChangeDirection()
+        {
+            var r = new Robot(0, 0, new NorthDirection());
+            new TurnRightCommand().Execute(r, null);
+            Assert.Equal('E', r.Direction.Name);
+            new TurnLeftCommand().Execute(r, null);
+            Assert.Equal('N', r.Direction.Name);
+        }
+
+        [Fact]
+        public void MoveCommand_MovesTheRobot()
+        {
+            var grid = new Grid(5, 5);
+            var r = new Robot(1, 1, new EastDirection());
+            new MoveForwardCommand().Execute(r, grid);
+            Assert.Equal((2, 1), (r.X, r.Y));
+        }
+    }
+}
