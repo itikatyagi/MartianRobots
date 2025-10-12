2b89255cc928cdbf3e4d11d14418b84569498a10
itikatyagi <itikatyagi60@gmail.com>
2025-10-12 02:59:37 +0100

    feat: add CLI runner + parser selection + validation

---
+++ MartianRobots/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using MartianRobots.Parsing;
+using MartianRobots.Runner;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // default: read stdin (extensible). If an input file path is provided, use that.
+        using var reader = args.Length > 0 ? new StreamReader(args[0]) : Console.In;
+        var parser = new SimpleTextParser();
+        var runner = new SimulationRunner(parser, Console.Out);
+        runner.Run(reader);
+    }
+}
---
+++ MartianRobots/Runner/SimulationRunner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using MartianRobots.Models;
+using MartianRobots.Models.Directions;
+using MartianRobots.Parsing;
+using MartianRobots.Commands;
+
+namespace MartianRobots.Runner
+{
+    /// <summary>Runs full simulation given an input parser and output writer.</summary>
+    public class SimulationRunner
+    {
+        private readonly IInputParser _parser;
+        private readonly TextWriter _output;
+        public SimulationRunner(IInputParser parser, TextWriter output)
+        {
+            _parser = parser;
+            _output = output;
+        }
+
+        public void Run(TextReader input)
+        {
+            var parsed = _parser.Parse(input);
+            var grid = new Grid(parsed.GridSize.X, parsed.GridSize.Y);
+
+            foreach (var r in parsed.Robots)
+            {
+                var dir = DirectionFactory.Create(r.Direction);
+                var robot = new Robot(r.X, r.Y, dir);
+                foreach (var c in r.Commands)
+                {
+                    if (robot.IsLost) break;
+                    var cmd = CommandFactory.Create(c);
+                    cmd.Execute(robot, grid);
+                }
+                _output.WriteLine(robot.ToString());
+            }
+        }
+    }
+}
