496b219b00f00b84b8a392f1bf50adecd36637dc
itikatyagi <itikatyagi60@gmail.com>
2025-10-13 01:11:19 +0100

    fix: validate directions and commands in SimpleTextParser and added test as well

--- MartianRobots/Parsing/SimpleTextParser.cs
+++ MartianRobots/Parsing/SimpleTextParser.cs
@@ -2,13 +2,15 @@
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
-using MartianRobots.Parsing;
 
 namespace MartianRobots.Parsing
 {
     /// <summary>Parses the classic Martian Robots textual format.</summary>
     public class SimpleTextParser : IInputParser
     {
+        private static readonly char[] ValidDirections = { 'N', 'E', 'S', 'W' };
+        private static readonly char[] ValidCommands = { 'L', 'R', 'F' };
+
         public ParseResult Parse(TextReader reader)
         {
             var lines = new List<string>();
@@ -30,9 +32,21 @@ namespace MartianRobots.Parsing
             {
                 var pos = lines[i].Split(' ', StringSplitOptions.RemoveEmptyEntries);
                 if (pos.Length != 3) throw new InvalidDataException($"Invalid robot position line: {lines[i]}");
-                int rx = int.Parse(pos[0]), ry = int.Parse(pos[1]);
-                char dir = pos[2][0];
+
+                int rx = int.Parse(pos[0]);
+                int ry = int.Parse(pos[1]);
+                char dir = char.ToUpperInvariant(pos[2][0]);
+
+                // Validate direction
+                if (!ValidDirections.Contains(dir))
+                    throw new ArgumentException($"Unknown direction '{dir}'");
+
                 var commands = lines[i + 1].Trim().ToUpperInvariant();
+
+                // Validate commands
+                if (commands.Any(c => !ValidCommands.Contains(c)))
+                    throw new ArgumentException($"Unknown command in sequence '{lines[i + 1]}'");
+
                 robots.Add(new RobotScenario(rx, ry, dir, commands));
             }
 
