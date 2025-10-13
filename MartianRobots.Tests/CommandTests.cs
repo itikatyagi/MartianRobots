bcb035a703b937393862322bbbf8858ee12eb878
itikatyagi <itikatyagi60@gmail.com>
2025-10-13 01:04:24 +0100

    test: update CommandTests to use valid grid and clarify MoveForwardCommand behavior

--- MartianRobots.Tests/CommandTests.cs
+++ MartianRobots.Tests/CommandTests.cs
@@ -25,5 +25,60 @@ namespace MartianRobots.Tests
             new MoveForwardCommand().Execute(r, grid);
             Assert.Equal((2, 1), (r.X, r.Y));
         }
+
+        [Fact]
+        public void MoveCommand_MarksRobotAsLost_WhenMovingOffGrid()
+        {
+            var grid = new Grid(2, 2);
+            var r = new Robot(2, 2, new NorthDirection());
+            new MoveForwardCommand().Execute(r, grid);
+            Assert.True(r.IsLost);
+            Assert.True(grid.HasScent(2, 2, 'N'));
+        }
+
+        [Fact]
+        public void MoveCommand_IgnoresMove_WhenScentExists()
+        {
+            var grid = new Grid(2, 2);
+            var r1 = new Robot(2, 2, new NorthDirection());
+            new MoveForwardCommand().Execute(r1, grid); // lost and scent added
+
+            var r2 = new Robot(2, 2, new NorthDirection());
+            new MoveForwardCommand().Execute(r2, grid); // should ignore move
+            Assert.False(r2.IsLost);
+            Assert.Equal((2, 2), (r2.X, r2.Y));
+        }
+
+        [Fact]
+        public void SequenceOfCommands_MovesRobotCorrectly()
+        {
+            var grid = new Grid(5, 5);
+            var r = new Robot(1, 1, new NorthDirection());
+            var commands = new ICommand[]
+            {
+                new MoveForwardCommand(),
+                new TurnRightCommand(),
+                new MoveForwardCommand(),
+                new TurnLeftCommand(),
+                new MoveForwardCommand()
+            };
+
+            foreach (var cmd in commands)
+                cmd.Execute(r, grid);
+
+            Assert.Equal((2, 3), (r.X, r.Y));
+            Assert.Equal('N', r.Direction.Name);
+        }
+
+        [Fact]
+        public void MoveForwardCommand_DoesNotThrow_WithValidGrid()
+        {
+            var r = new Robot(0, 0, new NorthDirection());
+            var grid = new Grid(5, 5);
+            var cmd = new MoveForwardCommand();
+            var ex = Record.Exception(() => cmd.Execute(r, grid));
+            Assert.Null(ex);
+        }
+
     }
 }
